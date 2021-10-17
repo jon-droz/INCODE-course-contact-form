a76d6d6c166f8f844fbf836672049727625b8f43
jon-droz <87005556+jon-droz@users.noreply.github.com>
2021-10-17 17:45:18 +0200

    Error message added

--- src/form.js
+++ src/form.js
@@ -1,58 +1,67 @@
-/*eslint-disable*/
-const form = document.querySelector("form");
-const textareaValid = document.querySelector("textarea");
-const inputName = form.querySelector("input[name=name]")
-const inputSurname = form.querySelector("input[name=surname]")
-const inputPhone = form.querySelector("input[name=phone]")
-const inputEmail = form.querySelector("input[name=email]")
-const textareaMessage = form.querySelector("textarea[name=message]")
+const form = document.querySelector('form');
+const inputName = form.querySelector('input[name=name]');
+const inputSurname = form.querySelector('input[name=surname]');
+const inputPhone = form.querySelector('input[name=phone]');
+const inputEmail = form.querySelector('input[name=email]');
+const textareaMessage = form.querySelector('textarea[name=message]');
 
-form.setAttribute("novalidate", true);
+form.setAttribute('novalidate', true);
 
-const phoneRegex = /^(\+)?(\d{2})?\s?\d{3}[ -]?\d{3}[ -]?\d{3}$/
-const emailRegex = /^[a-zA-Z1-9-_+.]+@[a-zA-Z1-9-_\.]+\.[a-zA-Z]+$/
+const phoneRegex = /^(\+)?(\d{2})?\s?\d{3}[ -]?\d{3}[ -]?\d{3}$/;
+const emailRegex = /^[a-zA-Z1-9-_+.]+@[a-zA-Z1-9-_.]+\.[a-zA-Z]+$/;
 
-const modal = document.querySelector("#modal")
-const closeModalButton = document.querySelector("#close-button")
+const errorMessage = document.createElement('div');
+errorMessage.className = 'errorMessage';
+errorMessage.style.margin = '40px 0';
+errorMessage.style.fontWeight = 'bold';
+form.append(errorMessage);
 
-function showModal () {
-    modal.style.display = "block";
+const modal = document.querySelector('#modal');
+const closeModalButton = document.querySelector('#close-button');
+
+function showModal() {
+  modal.style.display = 'block';
 }
 
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = 'none';
+  // form.submit();
 }
 
-closeModalButton.addEventListener("click", closeModal)
+closeModalButton.addEventListener('click', closeModal);
 
-function validation (event) {
-    event.preventDefault()
-    
-    if(inputName.value.length < 1) {
-        console.log("Error name");
-    } else if(inputSurname.value.length <1) {
-        console.log("Error surname");
-    } else if (inputPhone.value !== "" && !phoneRegex.test(inputPhone.value)) {
-        console.log("Error phone")
-    } else if (!emailRegex.test(inputEmail.value)) {
-        console.log("Error mail");
-    } else if (textareaMessage.value.length < 3) {
-        console.log("error message");
-    } else {
-        //form.submit();
-        showModal();
-        console.log(`Name: ${inputName.value}
+function validation(event) {
+  event.preventDefault();
+
+  const errorMessages = [];
+
+  if (inputName.value.length < 1) {
+    errorMessages.push('nom');
+  }
+  if (inputSurname.value.length < 1) {
+    errorMessages.push('prenom');
+  }
+  if (inputPhone.value !== '' && !phoneRegex.test(inputPhone.value)) {
+    errorMessages.push('telephone');
+  }
+  if (!emailRegex.test(inputEmail.value)) {
+    errorMessages.push('adresse mail');
+  }
+  if (textareaMessage.value.length < 3) {
+    errorMessages.push('message');
+  }
+
+  if (errorMessages.length > 0) {
+    errorMessage.innerHTML = `Les données saisies sont incorrectes ou incomplètes (${errorMessages.join(', ')}).`;
+  } else {
+    showModal();
+    console.log(`Name: ${inputName.value}
                      Surname: ${inputSurname.value}
                      Phone: ${inputPhone.value}
                      Email: ${inputEmail.value}
                      Message: ${textareaMessage.value}
                      `);
-    }
-    }
+  }
+}
 
-form.addEventListener("submit", validation);
-
-// form.addEventListener("submit", v => {
-//     v.preventDefault()
-// console.log("testowe wywołaine z funkcją strzałkową");
-// });
+form.addEventListener('submit', validation);
